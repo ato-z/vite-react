9e0dc41a5c33cc9b729a0202165d04cf621a7985
atoz <523333404@qq.com>
2024-04-17 15:35:44 +0800

    💐 异步加载

---
+++ src/components/loading/index.tsx
@@ -0,0 +1,25 @@
+import React, { Suspense } from 'react'
+import './index.scss'
+
+export const Loading = () => {
+  return (
+    <div className="loader">
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+    </div>
+  )
+}
+
+/**
+ * 包裹一个异步组件，加载成功前显示loading状态
+ */
+export default ({ children }: { children: React.ReactNode }) => {
+  return <Suspense fallback={<Loading />}>{children}</Suspense>
+}
